029b5d7ae323bf1552a699b87a6b7c8fd7817476
Taylor Hummon <other@taylorhummon.com>
2026-07-06 14:24:22 -0400

    update dial tests

--- tests/shared/utilities/dial.test.ts
+++ tests/shared/utilities/dial.test.ts
@@ -14,7 +14,7 @@ function exerciseNoteAt(
 function getPositions(
   musicalKey: MusicalKey,
 ): Array<number> {
-  const centerPosition =  musicalKey.mode
+  const centerPosition = - musicalKey.mode
   return buildInclusiveRange(centerPosition - 3, centerPosition + 3)
 }
 
@@ -23,16 +23,16 @@ test("noteAt() works", () => {
   expect(
     exerciseNoteAt(new MusicalKey({ mode: 0, root: 0 }))
   ).toStrictEqual(
-    [ "B", "E", "A", "D", "G", "C", "F" ]
+    [ "F", "C", "G", "D", "A", "E", "B" ]
   )
   expect(
     exerciseNoteAt(new MusicalKey({ mode: -2, root: 1 }))
   ).toStrictEqual(
-    [ "G♯", "C♯", "F♯", "B", "E", "A", "D" ]
+    [ "D", "A", "E", "B", "F♯", "C♯", "G♯" ]
   )
   expect(
     exerciseNoteAt(new MusicalKey({ mode: 1, root: -1 }))
   ).toStrictEqual(
-    [ "A", "D", "G", "C", "F", "B♭", "E♭" ]
+    [ "E♭", "B♭", "F", "C", "G", "D", "A" ]
   )
 })
