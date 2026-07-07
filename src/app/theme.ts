22fbfb59cd0aceb567cc4a99141218e22980ad3a
Taylor Hummon <other@taylorhummon.com>
2026-07-07 12:48:09 -0400

    reduce mantine small media query breakpoint

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -5,4 +5,11 @@ import { createTheme } from "@mantine/core"
 export const theme = createTheme({
   fontFamily: SANS_FONT.style.fontFamily,
   primaryColor: "yellow",
+  breakpoints: {
+    xs: "576px",
+    sm: "700px",
+    md: "992px",
+    lg: "1200px",
+    xl: "1408px",
+  },
 })
